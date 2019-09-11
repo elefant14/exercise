2483bd3d263d490c035dccf6b5f164ad326e9907
Joanna <elefant14@poczta.fm>
2019-09-12 00:27:50 +0200

    Ex7.1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import static java.util.Locale.ENGLISH;
+
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String textToBeautify = poemBeautifier.beautify("This text is beautiful", (decorateText ->  "ABC" + decorateText + "ABC"));
+        System.out.println(textToBeautify);
+
+        String textToBeautify1 = poemBeautifier.beautify("This text is beautiful", (decorateText ->decorateText.toUpperCase(ENGLISH)));
+        System.out.println(textToBeautify1);
+
+        String textToBeautify2 = poemBeautifier.beautify("This text is beautiful", (decorateText -> decorateText.trim()));
+        System.out.println(textToBeautify2);
+
+        String textToBeautify3 = poemBeautifier.beautify("This text is beautiful", (decorateText -> String.valueOf(((decorateText.startsWith("#Th"))))));
+        System.out.println(textToBeautify3);
     }
 }
