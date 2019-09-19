44e2c54b62f86c51cbe0912b1f1ee058c21a5ad5
Joanna <elefant14@poczta.fm>
2019-09-19 23:00:22 +0200

    Ex8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -3,17 +3,32 @@ package com.kodilla.exception.io;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() throws IOException {
+    public void readFile() {
+        Object getFile = null;
+        if (getFile !=null) {
+            System.out.println("Oh no! Sth went wrong");
+        } else {
+            return;
 
-            ClassLoader classLoader=getClass().getClassLoader();
-            File file=new File(classLoader.getResource("file/names.txt").getFile());
-        Path path = Paths.get(file.getPath());
-        Stream<String> fileLines = Files.lines(path);
-        System.out.println(file.getPath());
-    }
+        }
+
+
+        ClassLoader classLoader=getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/names.txt").getFile());
+
+        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){
+            fileLines.forEach(System.out::println);
+
+        } catch (IOException e) {
+            System.out.println("Óh no! Sth went wrong! Error:"+e);
+        } finally {
+            System.out.println("I am gonna to be here...always");
+        }
+
+}
+
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -2,10 +2,8 @@ package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
 
-import java.io.IOException;
-
 public class ExceptionModuleRunner {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         FileReader fileReader = new FileReader();
         fileReader.readFile();
     }
